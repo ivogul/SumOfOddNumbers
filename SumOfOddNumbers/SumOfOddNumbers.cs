aa9fe2d2392bf747adaf4e1ea1de73bbfbeefac8
ivogul <ivogul@gmail.com>
2017-09-28 01:28:44 +0300

    new repo

---
+++ SumOfOddNumbers/SumOfOddNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfOddNumbers
+{
+    class SumOfOddNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+            int sum = 0;
+            for (int i = 0; i < n; i ++)
+            {
+                Console.WriteLine(num);
+                sum += num;
+                num += 2;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
